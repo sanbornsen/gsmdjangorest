1a68f53bba379527086d19febc719123700aa65e
Sudipta Sen <sudipta.sen@gopassport.com>
2016-06-08 08:45:40 +0530

    Moving the ranking logic from models to views

--- gsmbackend/searchphone/models.py
+++ gsmbackend/searchphone/models.py
@@ -3,25 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 import pickle
-from operator import itemgetter
 # Create your models here.
 
 def searchByRank(params, number=10):
     f = open('data/data.pkl', 'rb')
     data = pickle.load(f)
     f.close()
-    for i in range(len(data)):
-    	data[i]['rank'] = calculateRank(params, data[i])
-
-    data = sorted(data, key=itemgetter('rank'), reverse=True)
-
-    return data[0:10]
-
-
-def calculateRank(params, data):
-	if data['price'] == 0 or data['price'] > float(params['price']):
-		return -1
-	return ((float(params['wc'])*data['camera']/8.0) +
-		(float(params['wb'])*data['battery']/2100.0) +
-		(float(params['wr'])*data['ram']/2.0) +
-		(float(params['ws'])*data['memory']/8.0))
+    return data
--- gsmbackend/searchphone/views.py
+++ gsmbackend/searchphone/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from operator import itemgetter
 from .models import searchByRank
 # Create your views here
 
@@ -15,5 +16,23 @@ def index(request):
             params[i] = str(request.query_params[i])
         else:
             params[i] = 0
-    data = searchByRank(params, 10)
-    return Response(data)
+    data = searchByRank(params)
+    newData = list()
+    for i in range(len(data)):
+        rank = calculateRank(params, data[i])
+        if rank >= 0:
+            data[i]['rank'] = rank
+            newData.append(data[i])
+
+    # Sorting the data based on rank
+    newData = sorted(newData, key=itemgetter('rank'), reverse=True)
+    return Response(newData[0:10])
+
+def calculateRank(params, data):
+    # Out of price bound or no price from the crawled data
+    if data['price'] == 0 or data['price'] > float(params['price']):
+        return -1
+    return ((float(params['wc'])*data['camera']/8.0) +
+        (float(params['wb'])*data['battery']/2100.0) +
+        (float(params['wr'])*data['ram']/2.0) +
+        (float(params['ws'])*data['memory']/8.0))
